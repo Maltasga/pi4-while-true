f2ed328a6a55d947a472efc8c0ed50479e865e1a
Gabriel Malta <gmalta44@gmail.com>
2017-09-16 22:38:22 -0300

    estrutura inicial de construcao do projeto

---
+++ kkcommerce/src/main/java/br/edu/senac/kkcommerce/KkcommerceApplication.java
@@ -0,0 +1,12 @@
+package br.edu.senac.kkcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkcommerceApplication.class, args);
+	}
+}
