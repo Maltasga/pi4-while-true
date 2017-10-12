4e84b58e18c8ebeebbcca6810aa0f2ccee361724
Gabriel Malta <gmalta44@gmail.com>
2017-10-12 11:21:03 -0300

    ajustar scripts para execução no mysql

--- Database/01 - Scripts/03. INSERT Produto teste.sql
+++ Database/01 - Scripts/03. INSERT Produto teste.sql
@@ -1,2 +1,2 @@
 INSERT INTO PRODUTO (NOME, DESCRICAO, ID_MARCA, ID_COLECAO, VALOR) 
-    VALUES ('produto teste', 'descrição do produto teste', 40, 20, 83.90);
+    VALUES ('produto teste', 'descrição do produto teste', 1, 1, 83.90);
