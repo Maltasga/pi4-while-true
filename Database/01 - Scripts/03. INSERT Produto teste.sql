a001e986d9388adaf6d369b4390bd899bd420f22
Gabriela <gabriela.leandro.ti@gmail.com>
2017-10-28 16:42:19 -0200

    Criação da tela de lista de produtos da loka, e o inicio dos detalhes

--- Database/01 - Scripts/03. INSERT Produto teste.sql
+++ Database/01 - Scripts/03. INSERT Produto teste.sql
@@ -1,6 +1,6 @@
 INSERT INTO PRODUTO (NOME, DESCRICAO, ID_MARCA, ID_COLECAO, VALOR) VALUES
-    ('produto teste', 'descrição do produto teste', 1, 1, 87.79),
-    ('produto teste 2', 'descrição do produto teste 2', 5, 3, 57.30),
-    ('produto teste 3', 'descrição do produto teste 3', 7, 3, 99.90);
-    ('produto teste 4', 'descrição do produto teste 3', 7, 3, 99.90);
-    ('produto teste 5', 'descrição do produto teste 3', 7, 3, 99.90);
+    ('produto teste', 'descricao do produto teste', 1, 1, 87.79),
+    ('produto teste 2', 'descricao do produto teste 2', 5, 3, 57.30),
+    ('produto teste 3', 'descricao do produto teste 3', 7, 3, 99.90),
+    ('produto teste 4', 'descricao do produto teste 3', 7, 3, 99.90),
+    ('produto teste 5', 'descricao do produto teste 3', 7, 3, 99.90);
